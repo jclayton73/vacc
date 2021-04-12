1bad2bdeb23597c4d35288f736dcab8d385752ef
Jackson Clayton <jackson.clayton@tufts.edu>
2021-04-12 16:50:57 -0400

    implement error 404 correction

--- findVaccAppt.py
+++ findVaccAppt.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 import sched
 import time
@@ -17,20 +17,26 @@ LOG = False
 
 def find_appt():
     driver.refresh()
-    WebDriverWait(driver, 5).until(
-        ec.presence_of_element_located((By.XPATH, "//figure/div"))
-    )
-    search_page()
+    try:
+        WebDriverWait(driver, 5).until(
+            ec.presence_of_element_located((By.XPATH, "//figure/div"))
+        )
+        search_page()
+    except TimeoutException:
+        if driver.title == "Oops, page not found - 404 Error - Zocdoc":
+            driver.get("https://www.zocdoc.com/wl/tuftscovid19vaccination/patientvaccine")
+            find_appt()
+        else:
+            raise Exception
 
 
 def search_page():
     global found
-    global t
     try:
         element = driver.find_element(By.XPATH, '//*[@ data-test="timesgrid-timeslot"]')
         print(f"\r\033[1m{time.ctime(time.time())}\033[0m --- "
               f"\033[32mappointment found!\033[0m\033[K\n"
-              f"{' ' * 24}{element.get_attribute('aria-label')}")
+              f"{' ' * 25}{element.get_attribute('aria-label')}")
         if LOG:
             f = open("findVaccLog.txt", "a")
             f.write(f"{time.ctime(time.time())}\n")
@@ -38,9 +44,6 @@ def search_page():
         return
     except NoSuchElementException:
         try:
-            # search for next appointment
-            #     if found click yes, scroll, click next appointment, search page
-
             element = driver.find_element(By.XPATH, '//*[@ data-test="next-availability-button"]')
             print(f"\r\033[1m{time.ctime(time.time())}\033[0m --- "
                   f"\033[33mnext appointment found\033[0m\033[K")
@@ -72,10 +75,9 @@ def strToDate(sdate):
             or int(sdate[2:4]) not in range(1, 31):
         raise TypeError
     return int(sdate)
-
-
 #     compare to current date
 #     make sure all numbers are within range
+
 
 def inDateRange(starttime, day, endday):
     intstarttime = int(starttime[0:10].replace("-", ""))
@@ -93,22 +95,24 @@ if __name__ == '__main__':
     parser.add_argument("--day", "-d", type=strToDate, help="find appointment for specific date - MMDDYYYY\n"
                                                             "if -e option is used, finds appointment between dates")
     parser.add_argument("-endday", "-e", type=strToDate, help="find appointment before date - MMDDYYYY")
+    parser.add_argument("-frequency", "-f", type=int, default=10, help="frequency to check appointments, in seconds")
     args = parser.parse_args()
 
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://www.zocdoc.com/wl/tuftscovid19vaccination/patientvaccine")
+    driver.get("https://www.zocdoc.com/wl/tuftscovid19vaccination/patientvaccineplease")
 
     s = sched.scheduler(time.time, time.sleep)
     found = ""
     t = get_time()
     LOG = args.log
+    FREQ = args.frequency
     while not found:
         s.enterabs(t + FREQ, 1, find_appt)
         s.run()
         if LOG:
-            found = None
+            found = ""
         if found:
             if args.day is not None:
                 if args.endday is not None:
